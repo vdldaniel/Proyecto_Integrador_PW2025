5582947658c3a4b96345675995b746452415453d
vdldaniel <vdldaniel09@gmail.com>
2025-09-27 19:57:07 -0300

    Se agrega mis-torneos-detalle.html

--- FutMatch/src/scripts/pages/adminInc.js
+++ FutMatch/src/scripts/pages/adminInc.js
@@ -93,9 +93,30 @@ function enviarFormularioTorneo() {
   }
 }
 
+// Cancelar la creacion del formulario nuevo torneo
 function cancelarFormularioTorneo() {
   if (confirm("¿Seguro que quieres cancelar la creación del torneo?")) {
     window.location.href = "mis-torneos.html"; // vuelve a la principal
   }
 }
 
+// Cancelar un torneo Activo o Cancelado
+function cancelarTorneo(){
+  if (confirm("¿Seguro que quieres cancelar el torneo?")) {
+    window.location.href = "mis-torneos.html"; // vuelve a la principal
+  }
+}
+
+// Botón visible de cancelar segun estado de Torneo
+document.addEventListener("DOMContentLoaded", () => {
+  const filas = document.querySelectorAll("table tbody tr");
+
+  filas.forEach(fila => {
+    const estado = fila.querySelector(".estado").textContent.trim();
+    const botonCancelar = fila.querySelector(".btn-danger");
+
+    if (estado === "Cancelado" && botonCancelar) {
+      botonCancelar.style.display = "none"; // lo oculta
+    }
+  });
+});
