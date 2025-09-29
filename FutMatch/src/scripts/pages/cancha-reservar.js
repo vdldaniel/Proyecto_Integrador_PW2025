230da05ef5f29eb1446a47ad25b25035ff5b0854
Facundo Alvarez <100171303+facu-alvarez98@users.noreply.github.com>
2025-09-28 21:59:40 -0300

    actualizacipmn
    script mapa cancha clasificar para ver las canchas de la zona y de paso clasificarlas si se quiere

--- FutMatch/src/scripts/pages/cancha-reservar.js
+++ FutMatch/src/scripts/pages/cancha-reservar.js
@@ -55,24 +55,24 @@
     const dificultad=document.getElementById('dificultad');
     
 
-   (() => {
-  'use strict'
+//    (() => {
+//   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//   const forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+//   // Loop over them and prevent submission
+//   Array.from(forms).forEach(form => {
+//     form.addEventListener('submit', event => {
+//       if (!form.checkValidity()) {
+//         event.preventDefault()
+//         event.stopPropagation()
+//       }
 
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+//       form.classList.add('was-validated')
+//     }, false)
+//   })
+// })()
 
    
 
